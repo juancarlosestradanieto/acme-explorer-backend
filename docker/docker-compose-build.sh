00f2728cb50dd7d3ccbb443ad2e4f20caf507afc
Juan Carlos Estrada Nieto <juancarlosestradanieto@gmail.com>
2023-04-14 02:15:28 +0200

    fix deploy script

--- docker/docker-compose-build.sh
+++ docker/docker-compose-build.sh
@@ -1,6 +1,8 @@
 # Deploy developement app
 docker compose -p "acme-explorer-api-dev" --env-file .env up --build -d
 
+chmod 777 -R .
+
 echo "Please wait 30 seconds while mongo db is initialized"
 sleep 30
 
