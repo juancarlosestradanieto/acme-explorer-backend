a40c986d1063f3e5eed8c0844c7a9bdf99f0b462
Juan Carlos Estrada Nieto <juancarlosestradanieto@gmail.com>
2023-04-26 02:07:02 +0200

    added header

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, idToken') // watch out, if a custom parameter, like idToken, is added in the header, it must to be declared here to avoid CORS error
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  res.header('Access-Control-Expose-Headers', '*')
   next()
 })
 
